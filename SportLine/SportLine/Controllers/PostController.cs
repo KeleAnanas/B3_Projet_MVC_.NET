e9ea9af93f3e871071bb12edd632b3bcf897bd92
akoan60 <anaelakue60@gmail.com>
2019-11-27 16:17:54 +0100

    ajout page details

--- SportLine/SportLine/Controllers/PostController.cs
+++ SportLine/SportLine/Controllers/PostController.cs
@@ -9,18 +9,31 @@ namespace SportLine.Controllers
 {
     public class PostController : Controller
     {
+        private List<Post> liste = new List<Post>()
+        {
+            new Post(1, "Coach de basket", "Nous avons besoin d'un coach de basket", "https://basket-image.jpg"),
+            new Post(2, "Hockey sur glace", "Nous avons besoin d'un coach de basket", "https://basket-image.jpg"),
+            new Post(3, "Foot en salle", "Nous avons besoin d'un coach de basket", "https://basket-image.jpg"),
+            new Post(4, "Sparring Judo", "Nous avons besoin d'un coach de basket", "https://basket-image.jpg"),
+            new Post(5, "Skateboard", "Nous avons besoin d'un coach de basket", "https://basket-image.jpg"),
+        };
+
         public IActionResult PostListe()
         {
-            var liste = new List<Post>()
-            {
-                new Post(1, "Coach de basket", "Nous avons besoin d'un coach de basket", "https://basket-image.jpg"),
-                new Post(2, "Hockey sur glace", "Nous avons besoin d'un coach de basket", "https://basket-image.jpg"),
-                new Post(3, "Foot en salle", "Nous avons besoin d'un coach de basket", "https://basket-image.jpg"),
-                new Post(4, "Sparring Judo", "Nous avons besoin d'un coach de basket", "https://basket-image.jpg"),
-                new Post(5, "Skateboard", "Nous avons besoin d'un coach de basket", "https://basket-image.jpg"),
-            };
             // Remplir
-            return View(liste);
+            return View(this.liste);
         }
+
+        public IActionResult DetailsPost(int id)
+        {
+            Post details = this.Liste.Find(x => x.Id == id);
+            return View(details);
+        }
+
+        public List<Post> Liste
+        {
+            get => this.liste;
+        }
+
     }
 }
