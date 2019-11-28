b31da8f9c88819df90eb2f0bfcd2c79d26f927f6
akoan60 <anaelakue60@gmail.com>
2019-11-28 11:32:57 +0100

    classe utilisateur

---
+++ SportLine/SportLine/Models/Utilisateur.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportLine.Models
+{
+    public class Utilisateur
+    {
+        private string id;
+        private string nom;
+
+        public Utilisateur(string nom)
+        {
+            this.nom = nom;
+        }
+    }
+}
